c1191a83d3d10650ad513f9ef85cabe10ed136e4
tommy-mor <thmorriss@gmail.com>
2024-03-12 20:15:28 -0400

    streaming works!

--- src/electric_starter_app/llm.clj
+++ src/electric_starter_app/llm.clj
@@ -4,7 +4,6 @@
             [missionary.core :as m]
             [selmer.parser :as sel]))
 
-
 (def oai-key (-> "secrets.edn" slurp read-string :openai :api-key))
 (def ant-key (-> "secrets.edn" slurp read-string :anthropic :api-key ))
 
@@ -40,43 +39,74 @@
       :body (json/parse-string true) :content first :text))
 
 (defn ask-ant-stream [q]
-  (fn [n t]
-    (n)
-    
-    (let [body (-> (client/post "https://api.anthropic.com/v1/messages"
-                                {:headers {"Content-Type" "application/json"
-                                           "x-api-key" ant-key
-                                           "anthropic-version" "2023-06-01"}
-                                 :as :stream
-                                 :content-type :json
-                                 :form-params {:model "claude-3-sonnet-20240229"
-                                               :messages [{:role "user" :content q}]
-                                               :max_tokens 100
-                                               :stream true}})
-                   :body)]
-      
-      (reify clojure.lang.IDeref
-        (deref [_] (let [_ (read-until-newlines body)
-                         bit (read-until-newlines body)
-                         bit (json/parse-string (second (clojure.string/split bit #":" 2)) true)]
-                     (def bit "data: {\"type\": \"message start\"}")
-                     (if (= (:type bit) "message_stop")
-                       (do (t) bit)
-                       (do (n) bit))))))))
+  (m/ap
+   (let [body (m/? (m/via m/blk
+                          (-> (client/post "https://api.anthropic.com/v1/messages"
+                                           {:headers      {"Content-Type"      "application/json"
+                                                           "x-api-key"         ant-key
+                                                           "anthropic-version" "2023-06-01"}
+                                            :as           :stream
+                                            :content-type :json
+                                            :form-params  {:model      "claude-3-sonnet-20240229"
+                                                           :messages   [{:role "user" :content q}]
+                                                           :max_tokens 100
+                                                           :stream     true}})
+                              :body)))]
+     (loop []
+       (m/? (m/via m/blk (read-until-newlines body)))
+       (let [bit (m/? (m/via m/blk (read-until-newlines body)))
+             bit (json/parse-string (second (clojure.string/split bit #":" 2)) true)]
+         (if (= (:type bit) "message_stop")
+           bit (m/amb bit (recur))))))))
 
-(m/? (m/reduce conj (ask-ant-stream "hello")))
+(defn collect
+  ([sofar next]
+   (case (:type next)
+     "content_block_start" (str sofar (-> next :content_block :text))
+     "content_block_delta" (str sofar (-> next :delta :text))
+     sofar))
+  
+  ([] ""))
+
+(defn test []
+  (m/ap
+   (let [x (m/?> (m/seed ["Hello" "World" "!"]))]
+     (m/? (m/sleep 1000))
+     x)))
 
 (comment
   (def r (ask-ant-stream "hello"))
-  (slurp r)
-  (m/? (m/reduce conj (m/ap (let [[_ msg] (m/?> (m/eduction (partition-all 2) r))]
-                              (when msg
-                                (json/parse-string (second (clojure.string/split msg #":" 2) true)))))))
-  (defn read-messages [flow]
-    (let [[_ msg] (m/? (m/reduce conj (m/eduction (partition-all 2) flow)))]
-      (json/parse-string (second (clojure.string/split msg #":" 2)) true)))
-  (defn llm-stream [q] (read-messages (ask-ant q)))
-  (read-messages r)
-  (read-until-newlines r)
-  (take 100 s)
-  (m/?> s))
+  (m/? (m/reduce collect (m/ap
+                          (m/?> (ask-ant-stream "hello")))))
+  
+  (m/? (m/reduce conj (m/ap
+                       (let [x (m/?> (m/seed ["Hello" "World" "!"]))]
+                         (m/? (m/sleep 1000))
+                         x))))
+  
+  (m/? (m/reduce conj (m/ap (let [x (m/?> (m/seed [1 2 3 4]))]
+                              (m/? (m/sleep 10))
+                              x))))
+
+  (m/? (m/reduce conj (m/ap
+                       (let [x (m/?> (m/seed ["Hello" "World" "!"]))]
+                         (Thread/sleep 1000)
+                         x))))
+  
+  (m/? (m/reduce conj (m/ap
+                       (println (m/?> (ask-ant-stream "what is the  meaning of life"))))))
+  
+  (m/? (m/reduce conj (m/ap
+                       (println (m/?> (ask-ant-stream "what is the  meaning of life"))))))
+  
+  (def it ((m/reduce #(prn %2) nil (ask-ant-stream "what is the meaning of life"))
+           (partial prn :ok)
+           (partial prn :ex)))
+  
+  
+  (m/? (m/reduce clct (ask-ant-stream "hello")))
+  (m/? (m/reduce conj (ask-ant-stream "hello"))))
+
+
+
+
--- src/electric_starter_app/main.cljc
+++ src/electric_starter_app/main.cljc
@@ -9,17 +9,30 @@
 
 ;; Saving this file will automatically recompile and update in your browser
 
-(def tagid 123)
+(e/defn Response [v]
+  (let [r (e/server (new (m/reductions llm/collect (llm/ask-ant-stream v))))]
+    (e/client
+     (dom/h4 (dom/text r)))))
+
+#_(e/defn Response [v]
+  (let [r (e/server (m/? (m/reduce conj (m/ap
+                                         (let [x (m/?> (m/seed ["Hello" "World" "!"]))]
+                                           (m/? (m/sleep 10))
+                                           x)))))]
+    (e/client
+     (dom/h4 (dom/text r)))))
 
 (e/defn Main [ring-request]
   (e/client
    (binding [dom/node js/document.body]
-     (let [!val (atom nil) val (e/watch !val)]
+     (let [!responses (atom []) responses (e/watch !responses)]
        (dom/input
         (dom/props {:placeholder "Type a message" :maxlength 100})
         (dom/on "keydown" (e/fn [e]
                             (when (= "Enter" (.-key e))
                               (when-some [v (empty->nil (.. e -target -value))]
                                 (set! (.-value dom/node) "")
-                                (reset! !val (e/server (llm/ask-ant v))))))))
-       (dom/h3 (dom/text (str "hello: " val)))))))
+                                (swap! !responses conj v))))))
+       (dom/h3 (dom/text (str responses)))
+       (e/for-by identity [v responses]
+                 (Response. v))))))
